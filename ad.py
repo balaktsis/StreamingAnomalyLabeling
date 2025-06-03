9bb5c0bba8b559499aef067a25ff15a14fa338f2
Christos Balaktsis <balaktsis@csd.auth.gr>
2025-06-03 15:39:02 +0300

    Implemented simple batching variant

--- ad.py
+++ ad.py
@@ -12,55 +12,67 @@ from TSB_UAD.models.sand import SAND
 from TSB_UAD.models.matrix_profile import MatrixProfile
 from StreamingBatchIForest import StreamingBatchIForest
 
+
 public_root = "./data/"
-selected_domains = ['NASA-MSL', 'IOPS', 'Genesis', 'YAHOO']
+selected_domain = 'YAHOO'
 
-
-### Load the data
-# For each domain, pick the most anomalous ts
+# Load all time series from the YAHOO domain
 series_list, label_list = [], []
-for dom in selected_domains:
-    dom_path = os.path.join(public_root, dom)
-    files = [f for f in os.listdir(dom_path) if f.endswith('.out')]
-    
-    max_ones = -1
-    chosen = None
+dom_path = os.path.join(public_root, selected_domain)
+files = sorted(f for f in os.listdir(dom_path) if f.endswith('.out'))  # Ensure consistent ordering
 
-    for f in files:
-        file_path = os.path.join(dom_path, f)
-        df = pd.read_csv(file_path, header=None).dropna()
-        ones_count = (df.iloc[:, 1] == 1).sum()
 
-        if ones_count > max_ones:
-            max_ones = ones_count
-            chosen = f
+i=0
+for f in files:
+    file_path = os.path.join(dom_path, f)
+    df = pd.read_csv(file_path, header=None).dropna()
+    data = df.iloc[:, 0].astype(float).to_numpy()
+    label = df.iloc[:, 1].astype(int).to_numpy()
+    series_list.append(data)
+    label_list.append(label)
+    i+=1
+    if i == 4:  
+        break
 
-    if chosen:
-        df = pd.read_csv(os.path.join(dom_path, chosen), header=None).dropna().to_numpy()
-        data = df[:, 0].astype(float)
-        label = df[:, 1].astype(int)
-        series_list.append(data)
-        label_list.append(label)
 
-norm1_ts = series_list[0]
-norm1_labels = label_list[0]
-norm2_ts = np.concatenate([series_list[0],series_list[1]], axis=0)
-norm2_labels = np.concatenate([label_list[0],label_list[1]], axis=0)
-norm3_ts = np.concatenate([series_list[0],series_list[1],series_list[2]], axis=0)
-norm3_labels = np.concatenate([label_list[0],label_list[1],label_list[2]], axis=0)
-norm4_ts = np.concatenate(series_list, axis=0)
-norm4_labels = np.concatenate(label_list, axis=0)
+# Create cumulative time series and labels
+series = []
+labels = []
+
+for i in range(1, len(series_list) + 1):
+    cum_ts = np.concatenate(series_list[:i], axis=0)
+    cum_labels = np.concatenate(label_list[:i], axis=0)
+    series.append(cum_ts)
+    labels.append(cum_labels)
+
+
+norm1_ts = series[0]
+norm1_labels = labels[0]
+
+norm2_ts = series[1] if len(series) > 1 else None
+norm2_labels = labels[1] if len(labels) > 1 else None
+
+norm3_ts = series[2] if len(series) > 2 else None
+norm3_labels = labels[2] if len(labels) > 2 else None
+
+norm4_ts = series[3] if len(series) > 3 else None
+norm4_labels = labels[3] if len(labels) > 3 else None
 
 series = [norm1_ts, norm2_ts, norm3_ts, norm4_ts]
 labels = [norm1_labels, norm2_labels, norm3_labels, norm4_labels]
 
 
-### Offline 1 - IForest
-modelName='IForest'
+
 norm = 2
 data = series[norm-1]
 label = labels[norm-1]
 
+
+
+## Offline
+
+### Offline 1 - IForest
+modelName='IForest'
 slidingWindow = find_length(data)
 X_data = Window(window = slidingWindow).convert(data).to_numpy()
 
@@ -76,10 +88,6 @@ plt.savefig("Norm-"+str(norm)+"-"+modelName, dpi=300, bbox_inches='tight')
 
 ### Offline 2 - MatrixProfile
 modelName='MatrixProfile'
-norm = 2
-data = series[norm-1]
-label = labels[norm-1]
-
 slidingWindow = find_length(data)
 X_data = data
 
@@ -95,10 +103,6 @@ plt.savefig("Norm-"+str(norm)+"-"+modelName, dpi=300, bbox_inches='tight')
 
 ### Offline 3 - SAND
 modelName='SAND (offline)'
-norm = 2
-data = series[norm-1]
-label = labels[norm-1]
-
 slidingWindow = find_length(data)
 X_data = data
 
@@ -111,9 +115,54 @@ plotFig(data, label, score, slidingWindow, fileName="Norm-"+str(norm)+"-"+modelN
 plt.savefig("Norm-"+str(norm)+"-"+modelName, dpi=300, bbox_inches='tight')
 
 
-### Online 1 - SAND
+
+## Variant 1 - Batching
+batch_size = 100
+batches = [data[i:i + batch_size] for i in range(0, len(data), batch_size)]
+
+### Batch 1 - IForest
 modelName='SAND (online)'
-norm = 2
+for i, batch in enumerate(batches):
+    print(batch.shape)
+    slidingWindow = find_length(batch) # Is it OK to recompute the sliding window length for each batch?
+    X_data = Window(window = slidingWindow).convert(batch).to_numpy()
+    clf = IForest(n_jobs=1)
+    clf.fit(X_data)
+    score = clf.decision_scores_
+    score = MinMaxScaler(feature_range=(0,1)).fit_transform(score.reshape(-1,1)).ravel()
+    
+    if i == 0:
+        final_score = np.array([score[0]]*math.ceil((slidingWindow-1)/2) + list(score) + [score[-1]]*((slidingWindow-1)//2))
+    else:
+        final_score = np.concatenate((final_score, score))
+    print(f"Batch {i+1}/{len(batches)} processed.")
+
+plotFig(data, label, final_score, slidingWindow, fileName="Norm-"+str(norm)+"-"+modelName, modelName=modelName)
+plt.savefig("Norm-"+str(norm)+"-"+modelName, dpi=300, bbox_inches='tight')
+
+
+### Batch 2 - MatrixProfile
+modelName='MatrixProfile (online)'
+for i, batch in enumerate(batches):
+    slidingWindow = find_length(batch) # Is it OK to recompute the sliding window length for each batch?
+    X_data = batch
+    clf = MatrixProfile(window = slidingWindow)
+    clf.fit(X_data)
+    score = clf.decision_scores_
+    score = MinMaxScaler(feature_range=(0,1)).fit_transform(score.reshape(-1,1)).ravel()
+    
+    if i == 0:
+        final_score = np.array([score[0]]*math.ceil((slidingWindow-1)/2) + list(score) + [score[-1]]*((slidingWindow-1)//2))
+    else:
+        final_score = np.concatenate((final_score, score))
+    print(f"Batch {i+1}/{len(batches)} processed.")
+
+plotFig(data, label, final_score, slidingWindow, fileName="Norm-"+str(norm)+"-"+modelName, modelName=modelName)
+plt.savefig("Norm-"+str(norm)+"-"+modelName, dpi=300, bbox_inches='tight')
+
+
+### Batch 3 - SAND
+modelName='SAND (online)'
 data = series[norm-1]
 label = labels[norm-1]
 
@@ -128,17 +177,20 @@ plotFig(data, label, score, slidingWindow, fileName="Norm-"+str(norm)+"-"+modelN
 plt.savefig("Norm-"+str(norm)+"-"+modelName, dpi=300, bbox_inches='tight')
 
 
-### Online 2 - IForest
-sb_if = StreamingBatchIForest(
-    batch_frac=0.1,
-    overlap=10,
-    n_clusters=4,
-    state_size=None,       
-    tabpfn_device='cpu'
-)
-scores = sb_if.process(data)
 
-sliding = find_length(data)
-plotFig(data, label, scores, sliding,
-        fileName='Stream_IForest', modelName='StreamIForest')
-plt.savefig('Stream_IForest.png', dpi=300, bbox_inches='tight')
+# Variant 2 - Streaming
+
+# ### Online 2 - IForest
+# sb_if = StreamingBatchIForest(
+#     batch_frac=0.1,
+#     overlap=10,
+#     n_clusters=4,
+#     state_size=None,       
+#     tabpfn_device='cpu'
+# )
+# scores = sb_if.process(data)
+
+# sliding = find_length(data)
+# plotFig(data, label, scores, sliding,
+#         fileName='Stream_IForest', modelName='StreamIForest')
+# plt.savefig('Stream_IForest.png', dpi=300, bbox_inches='tight')
